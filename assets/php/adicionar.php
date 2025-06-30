b20e945ff32740685c466906597160ef0631deaf
Matheus Kauan Dums <matheus.dums@buddemeyer.local>
2025-06-30 13:00:18 -0300

    cosntruindo o CRUD

---
+++ assets/php/adicionar.php
@@ -0,0 +1,45 @@
+<?php
+include "conector.php";
+$erro = array();
+$saida = "";
+
+$nome = addslashes($_POST['nome']);
+$telefone = addslashes($_POST['telefone']);
+$email = addslashes($_POST['email']);
+$nascimento = addslashes($_POST['nascimento']);
+$observacoes = addslashes($_POST['observacoes']);
+
+if(empty($nome)) { /* verifica se um campo tá vazio, parece muito com o required do html, mas aqui ele apresenta o erro na tela */
+    $erro['e'] = "Digite o seu nome";
+}elseif(empty($telefone)) {
+    $erro['e'] = "Digite o seu telefone";
+}elseif(empty($email)) {
+    $erro['e'] = "Digite o seu email";
+}elseif(empty($nascimento)) {
+    $erro['e'] = "Digite a sua data de nascimento";
+}elseif(empty($observacoes)) {
+    $erro['e'] = "Digite as suas observacoes";
+} else {
+    $sql = "INSERT INTO tb_contatos (con_nome, con_telefone, con_email, con_nascimento, con_observacoes) 
+        VALUE(:nm, :tf, :em, :ns, :ob)";
+    $cmd = $pdo->prepare($sql);
+    $cmd->bindValue(":nm", $nome);
+    $cmd->bindValue(":tf", $telefone);
+    $cmd->bindValue(":em", $email);
+    $cmd->bindValue(":ns", $nascimento);
+    $cmd->bindValue(":ob", $observacoes);
+    $cmd->execute();
+    if($cmd->rowCount() >= 1) {
+        $saida .= "<p class='alert alert-success text-center'><strong>$nome</strong> cadastrado com sucesso!</p>";
+    } else {
+        $saida .= "<p class='alert alert-danger text-center'>Falha no cadastro.</p>";
+    }
+    
+}
+
+if(isset($erro['e'])) { /* caso esteja um campo sem preencher, irá aparecer o erro */
+    $saida .= "<p class='alert alert-danger text-center'>" . $erro['e'] . "</p>";
+} 
+
+echo $saida; /* para enviar ao jquery */
+?>
---
+++ assets/php/select.php
@@ -0,0 +1,77 @@
+<?php
+include "conector.php";
+
+/* Para inserir os dados do banco de dados na tabela */
+$base = "";
+
+if(isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $sql = "SELECT con_id, con_nome, con_telefone, con_email, con_nascimento, con_observacoes FROM tb_contatos WHERE con_id = :id";
+    $cmd = $pdo->prepare($sql);
+    $cmd->bindValue(":id", $id);
+    $cmd->execute();
+    $dados = $cmd->fetch();
+
+    $base .= '
+    <form action="" method="POST" id="form" class="form-group">
+        <input type="hidden" id="id" name="id" value="">
+            <div class="col">
+                <label for="nome" class="form-label">Nome</label>
+                <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome" value="'. $dados['con_nome'] .'">
+            </div>
+            <div class="col">
+                <label for="telefone" class="form-label">Telefone</label>
+                <input type="text" class="form-control" name="telefone" id="telefone" placeholder="Telefone" value="'. $dados['con_telefone'] .'">
+            </div>
+        
+            <div class="row pt-3">
+            <div class="col">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" id="email" placeholder="exemplo@email.com" value="'. $dados['con_email'] .'">
+            </div>
+            <div class="col">
+                <label for="nascimento" class="form-label">Nascimento</label>
+               <input type="date" class="form-control" name="nascimento" id="nascimento" value="'. $dados['con_nascimento'] .'">
+            </div>
+            </div>
+
+            <div class="row pt-4">
+                <label for="observacoes" class="form-label">Observações</label>
+                <textarea class="form-control" id="observacoes" name="observacoes" rows="3" value="'. $dados['con_observacoes'] .'"></textarea>
+            </div>
+
+            <div class="row pt-4">
+            <div class="col">
+                <input id="salvarCadastro" class="btn btn-success col-12" type="submit" value="Salvar">
+            </div>
+
+            <div class="col">
+                <input class="btn btn-danger col-12" value="Excluir">
+            </div>
+        </div>
+    </form>
+    ';
+} else {
+    $sql = "SELECT con_id, con_nome, con_telefone, con_email, con_nascimento, con_observacoes FROM tb_contatos";
+    $cmd = $pdo->prepare($sql);
+    $cmd->execute();
+    $dados = $cmd->fetchAll(PDO::FETCH_ASSOC);
+/*     var_dump($dados); */
+
+    foreach($dados as $dados_ct)
+    $base .= '
+        <tr>
+            <td scope="col">'. $dados_ct['con_nome'] .'</td>
+            <td scope="col">'. $dados_ct['con_telefone'] .'</td>
+            <td scope="col">'. $dados_ct['con_email'] .'</td>
+            <td scope="col">'. $dados_ct['con_nascimento'] .'</td>
+            <td scope="col">'. $dados_ct['con_observacoes'] .'</td>
+            <td><input type="submit" name="editar" id="'. $dados_ct['con_id'] .'" value="Editar" class="btn btn-primary editar"></td>
+            <td><input type="submit" name="excluir" id="'. $dados_ct['con_id'] .'" value="Excluir" class="btn btn-danger excluir"></td>
+        </tr>
+    ';
+}
+
+echo $base;
+
+?>
